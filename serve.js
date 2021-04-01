c27845f2bde92322c7c9b5a186610f5c366c07f4
lucas.leveneur <lucas.leveneur@nautilus.fr>
2021-04-01 10:59:32 +0400

    update serve

--- serve.js
+++ serve.js
@@ -3,7 +3,9 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+const port = 8080;
 app.listen(port, ()=> {
     console.log("serve listening");
 });
+
+console.log('ok');
